d0a1e1cc2b677c73c97c09342714de4f0ffcf60f
garyande1504 <175570878+garyande1504@users.noreply.github.com>
2024-09-19 12:54:02 -0700

    implement dictionary reshaping

--- src/openbim/csi/config.py
+++ src/openbim/csi/config.py
@@ -1,5 +1,62 @@
 
-enhancements = [
-    {"type": "Frame", "name": 32, "apply": ["Geometry.PDelta"]},
-    {"type": "Frame", "name": 33, "apply": ["Geometry.PDelta"]}
-]
+
+def reshape(apply):
+    """
+    apply is a list of dictionaries describing changes to apply to a model. 
+    This function reshapes this list into a form that is more efficient for processing. 
+
+    In particular, we want something like:
+
+    {
+        "Frame": {
+            <name>: [<applied changes>]
+        },
+        "Shell": {},
+        "Solid": {}
+    }
+    """
+
+    
+    reshaped = {
+        "Frame": {},
+        "Shell": {},
+        "Solid": {}
+    }
+
+    for change in apply:
+        typ = change["type"]
+        type_dict = reshaped[typ]
+        name = change["name"]
+        if name in type_dict:
+            type_dict[name].extend(change["apply"])
+        else :
+            type_dict[name] = change["apply"]
+
+    return reshaped
+
+
+if __name__ == "__main__":
+
+    apply = [
+        {"type": "Frame", "name": 32, "apply": ["Geometry.PDelta"]},
+        {"type": "Frame", "name": 32, "apply": ["Material.Plasticity"]},
+        {"type": "Frame", "name": 33, "apply": ["Material.Plasticity", "Geometry.PDelta"]}
+    ]
+
+    reshaped = reshape(apply)
+
+    print(reshaped)
+
+
+# Output should look something like:
+# 
+#   reshaped = {
+#        "Frame": {32: ["Geometry.PDelta", "Material.Plasticity"], 33: []},
+#   }
+
+
+# reshaped["Frame"][32] == ["Geometry.PDelta", "Material.Plasticity"]
+
+# for change in apply:
+#             if change["type"] == "Frame" and change["name"] == frame["Frame"]:
+#                 changes_for_current_elem.extend(change["apply"])
